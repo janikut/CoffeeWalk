1280c9cce92d4f3e8d35252ec54d07904a3f9387
Janina Kutyn <janina@getkeepsafe.com>
2019-04-04 12:38:25 +0200

    Updated color scheme

--- CoffeeWalk/Theme.swift
+++ CoffeeWalk/Theme.swift
@@ -10,9 +10,12 @@ import UIKit
 
 struct Theme {
     
-    static let tintColor = UIColor(red: 59.0/255.0, green: 26.0/255.0, blue: 11.0/255.0, alpha: 1.0)
-    static let accentColor1 = UIColor(red: 242.0/255.0, green: 106.0/255.0, blue: 44.0/255.0, alpha: 1.0)
-    static let accentColor2 = UIColor(red: 11.0/255.0, green: 179.0/255.0, blue: 162.0/255.0, alpha: 1.0)
-    static let accentColor3 = UIColor(red: 255.0/255.0, green: 219.0/255.0, blue: 34.0/255.0, alpha: 1.0)
+    static let tintColor = UIColor(named: "tint")!
+    
+    static let accentColor1 = UIColor(named: "accent1")!
+    
+    static let accentColor2 = UIColor(named: "accent2")!
+
+    static let accentColor3 = UIColor(named: "accent3")!
     
 }
